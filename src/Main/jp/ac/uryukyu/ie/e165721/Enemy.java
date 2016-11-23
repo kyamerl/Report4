d67c2ec2815ba678f467ef3b88427a04285dc184
Yuichi Kamezawa <e165721@ie.u-ryukyu.ac.jp>
2016-11-23 23:04:51 +0900

    commit & push Enemy failed

--- src/Main/jp/ac/uryukyu/ie/e165721/Enemy.java
+++ src/Main/jp/ac/uryukyu/ie/e165721/Enemy.java
@@ -51,7 +51,7 @@ public class Enemy {
      * @param hero 攻撃対象
      */
     public void attack(Hero hero){
-        if(dead == false) {
+        while(dead == false) {
             int damage = (int) (Math.random() * attack);
             System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
             hero.wounded(damage);
