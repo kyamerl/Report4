66f03e0f6913fa986778d3d46da3708c84fc989d
Yuichi Kamezawa <e165721@ie.u-ryukyu.ac.jp>
2016-11-27 19:41:43 +0900

    commit & push Enemy failed

--- src/test/jp/ac/uryukyu/ie/e165721/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/e165721/EnemyTest.java
@@ -2,7 +2,7 @@ package jp.ac.uryukyu.ie.e165721;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by e165721 on 2016/11/22.
@@ -19,11 +19,12 @@ public class EnemyTest {
         int heroHP = 10;
         Hero hero = new Hero("テスト勇者", heroHP, 5);
         Enemy enemy = new Enemy("テストスライム", 6, 3);
-        enemy.dead = true;
+        enemy.setDead(true);
         for(int i=0; i<10; i++) {
             enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
         }
-        assertEquals(heroHP, hero.hitPoint);
+        assertEquals(heroHP, hero.getHitPoint());
     }
+
 
 }
