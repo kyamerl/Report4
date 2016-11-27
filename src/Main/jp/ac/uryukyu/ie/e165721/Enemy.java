504f4ff25233bb672c7e896fdc144decb46fd2a1
Yuichi Kamezawa <e165721@ie.u-ryukyu.ac.jp>
2016-11-27 19:41:00 +0900

    commit & push Enemy failed

--- src/Main/jp/ac/uryukyu/ie/e165721/Enemy.java
+++ src/Main/jp/ac/uryukyu/ie/e165721/Enemy.java
@@ -23,5 +23,15 @@ public class Enemy extends LivingThing{
      */
     public Enemy (String name, int maximumHP, int attack) {
         super(name, maximumHP, attack);
+        //super();
+    }
+
+    @Override
+    public void wounded(int damage){
+        setHitPoint(getHitPoint()-damage);
+        if( getHitPoint() < 0 ) {
+            setDead(true);
+            System.out.printf("モンスター%sは倒れた。\n", getName());
+        }
     }
 }
